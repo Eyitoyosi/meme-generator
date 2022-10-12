af856131171f52fe31e71d2d82953a2b0ec47be5
eyitoyosi <harlahbee@gmail.com>
2022-10-12 17:21:53 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Meme from "./Components/Meme"
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <img src="https://www.kindpng.com/picc/m/47-470110_vadivelu-rage-comic-sarcasm-joke-comedian-yao-ming.png" alt="" 
+            className="header--image"/>
+            <h2 className="header--title">Glade Meme Generator</h2>
+        </div>
+     );
+}
+ 
+export default Navbar;
